53774b0357514355abd9fcc548f2289f6bb7737a
Denis Zgonjanin <deniszgonjanin@gmail.com>
2015-11-18 14:21:05 -0500

    simple model for comments

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Comment(models.Model):
+    comment = models.CharField(max_length=140)
+    datetime = models.DateTimeField('date published', auto_now=True)
