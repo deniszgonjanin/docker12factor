3373f55b98e988884de2892cb1d8764ecc9955ce
Denis Zgonjanin <deniszgonjanin@gmail.com>
2015-11-18 14:28:23 -0500

    migration

---
+++ comments/migrations/0001_initial.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Comment',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('comment', models.CharField(max_length=140)),
+                ('datetime', models.DateTimeField(auto_now=True, verbose_name=b'date published')),
+            ],
+        ),
+    ]
