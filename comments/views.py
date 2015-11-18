aebcc172c70969d6dd435ff760091c24654315c7
Denis Zgonjanin <deniszgonjanin@gmail.com>
2015-11-18 14:58:01 -0500

    a simple view and template

--- comments/views.py
+++ comments/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Comment
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    comments = Comment.objects.order_by('-datetime')
+    context = {'comments': comments}
+    
+    return render(request, 'index.html', context)
